8a43de3f40cd3714e6a7332e38344f35d6fe9b05
Salma Farid <79086386+SalmaFarid998@users.noreply.github.com>
2024-03-22 23:26:21 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed("#typing", {
+  strings: ["Larry Daniels ^2000", "Designer ^2000", "Developer ^2000"],
+  typeSpeed: 40,
+  loop: true,
+  loopCount: Infinity,
+  backSpeed: 40,
+});
